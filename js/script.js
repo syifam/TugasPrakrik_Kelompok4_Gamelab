a12173449001d66cbb31f944a91af348c38f6cea
Syifa <syifasandriyani@gmail.com>
2023-09-27 10:55:12 +0700

    update file js

--- js/script.js
+++ js/script.js
@@ -1,18 +1,17 @@
 // Script modal
 $(document).ready(function() {
-    $("#form-login").click(function(event) {
-      alert("Login berhasil");
-      event.preventDefault();
-    });
-  
-    $("#form-faq").click(function(event) {
-      alert("Data tersimpan");
-      event.preventDefault();
-    });
-  
-    $("#form-contact-us").click(function(event) {
-      alert("Pesan Terkirim");
-      event.preventDefault();
-    });
+  $("#form-login").on("submit", function(event) {
+    alert("Login berhasil");
+    event.preventDefault();
+  });
+
+  $("#form-faq").on("submit", function(event) {
+    alert("Data tersimpan");
+    event.preventDefault();
+  });
+
+  $("#form-contact-us").on("submit", function(event) {
+    alert("Pesan Terkirim");
+    event.preventDefault();
+  });
 });
-  
