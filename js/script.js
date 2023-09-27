ff79674f91d7c8ed1aa32e3dbff86b48195351b2
Syifa <syifasandriyani@gmail.com>
2023-09-27 10:04:29 +0700

    test

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// Script modal
+$(document).ready(function() {
+    $("#form-login").click(function(event) {
+      alert("Login berhasil");
+      event.preventDefault();
+    });
+  
+    $("#form-faq").click(function(event) {
+      alert("Data tersimpan");
+      event.preventDefault();
+    });
+  
+    $("#form-contact-us").click(function(event) {
+      alert("Pesan Terkirim");
+      event.preventDefault();
+    });
+});
+  
